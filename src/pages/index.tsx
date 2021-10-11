b59637a45699f4415574cb7fa04682529991f521
Alex Miller <ajm9594@gmail.com>
2021-10-11 14:19:37 -0700

    font integration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,10 @@ import * as React from "react";
 
 const IndexPage = () => {
     return(
-        <h1>HOI4 Production Manager</h1>
+        <main>
+            <h1 className="font-display">HOI4 Production Manager</h1>
+            <p className="font-body">Some body text</p>
+        </main>
     );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'display': ['Josefin Sans', 'Helvetica', 'Arial', 'sans-serif'],
+      'body': ['Montserrat', 'Helvetica', 'Arial', 'sans-serif']
+    },
     extend: {},
   },
   variants: {
