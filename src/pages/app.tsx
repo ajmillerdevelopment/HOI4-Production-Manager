743ce58a4621bb43bbf87eaa0da6ee49a35e0908
Alex Miller <ajm9594@gmail.com>
2021-10-16 10:22:18 -0700

    app placeholder

---
+++ src/pages/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return(
+        <div id="root" className="bg-black h-screen w-full fixed">
+            <h1 className="font-display text-white text-4xl text-center m-8">HOI4 Production Manager</h1>
+            <main className="bg-gray-900 w-full h-screen fixed mt-10 border-2 border-gray-100"></main>
+        </div>
+    )
+};
+
+export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,16 @@ import * as React from "react";
 
 const IndexPage = () => {
     return(
-        <main>
-            <h1 className="font-display">HOI4 Production Manager</h1>
-            <p className="font-body">Some body text</p>
+        <main className="bg-black h-screen w-full fixed">
+            <h1 className="font-display text-white text-7xl text-center m-8">HOI4 Production Manager</h1>
+            <div className="flex justify-center flex-row">
+                <a href="/#" className="p-4 m-2 border-2 bg-gray-900 border-gray-200">
+                    <p className="font-display text-white">Log In or Create Account</p>
+                </a>
+                <a href="/app" className="p-4 m-2 border-2 bg-gray-900 border-gray-200">
+                    <p className="font-display text-white">Use as Guest</p>
+                </a>
+            </div>
         </main>
     );
 };
