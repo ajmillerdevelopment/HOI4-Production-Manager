77e1ee312415ab61fed4222fa7de4c6cbbef5fb6
Alex Miller <ajm9594@gmail.com>
2021-11-13 19:39:54 -0800

    remove tests

--- src/store/Store.spec.ts
+++
@@ -1,9 +0,0 @@
-import Store from './Store';
-
-it("tests Store instantiation", () => {
-    const store = new Store(false);
-    expect(store.tags).toBe([]);
-    expect(store.units).toBe([]);
-    expect(store.templates).toBe([]);
-    expect(store.equipment).toBe([]);
-});
